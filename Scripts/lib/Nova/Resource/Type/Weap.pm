341024f4f76f02d0db9f2db83998fa7299cc6a47
vasi <vasi@6ae7a69f-d105-0410-9ab8-d01db65f754a>
2006-05-16 07:06:39 +0000

    er, keep the necessary optimizations

--- Scripts/lib/Nova/Resource/Type/Weap.pm
+++ Scripts/lib/Nova/Resource/Type/Weap.pm
@@ -7,6 +7,25 @@ use base 'Nova::Resource';
 __PACKAGE__->register('weap');
 
 use Nova::Resource::Type::Outf;
+use Nova::Cache;
+
+# Weapons to outfits cache
+sub _w2o {
+	$_[0]->precalc(w2o => sub {
+		my ($self, $cache) = @_;
+		for my $outf (reverse $self->collection->type('outf')) {
+			my $mass = $outf->mass;
+			for my $mod ($outf->mods) {
+				my $mv = $mod->{ModVal};
+				if ($mod->{ModType} == MT_WEAPON) {
+					$cache->{$mv}->{weapon} = $mass;
+				} elsif ($mod->{ModType} == MT_AMMO) {
+					$cache->{$mv}->{ammo} = $mass;
+				}
+			}
+		}
+	});
+}
 
 # What weapon does the ammo come from?
 sub ammoSource {
@@ -29,15 +48,12 @@ sub mass {
 	my ($self, $verb) = @_;
 	$verb = 0 unless defined $verb;
 	
-	for my $outf (reverse $self->collection->type('outf')) {
-		for my $mod ($outf->mods) {
-			next unless $mod->{ModType} == MT_WEAPON;
-			return $outf->mass if $mod->{ModVal} == $self->ID;
-		}
+	my $w2o = $self->_w2o;
+	unless (exists $w2o->{$self->ID}->{weapon}) {
+		warn sprintf "No outfit found for weapon ID %d\n", $self->ID if $verb;
+		return 0;
 	}
-
-	warn sprintf "No outfit found for weapon ID %d\n", $self->ID if $verb;
-	return 0;
+	return $w2o->{$self->ID}->{weapon};
 }
 
 # How much mass per ammo?
@@ -48,15 +64,12 @@ sub ammoMass {
 	my $source = $self->ammoSource;
 	return 0 unless defined $source;
 	
-	for my $outf (reverse $self->collection->type('outf')) {
-		for my $mod ($outf->mods) {
-			next unless $mod->{ModType} == MT_AMMO;
-			return $outf->mass if $mod->{ModVal} == $source->ID;
-		}
+	my $w2o = $self->_w2o;
+	unless (exists $w2o->{$source->ID}->{ammo}) {
+		warn sprintf "No outfit found for ammo ID %d\n", $self->ID if $verb;
+		return 0;
 	}
-
-	warn sprintf "No outfit found for ammo ID %d\n", $self->ID if $verb;
-	return 0;
+	return $w2o->{$source->ID}->{ammo};
 }
 
 

6705e8f9e60aa11fa1245ccee9bed761b56f0181
vasi <vasi@6ae7a69f-d105-0410-9ab8-d01db65f754a>
2012-06-13 09:07:30 +0000

    Print dude ship ID

--- Scripts/old.pl
+++ Scripts/old.pl
@@ -1538,7 +1538,8 @@ sub dude {
 	printf "Dude %d: %s\n", $dudeid, $dude->{Name};
 	my $ships = resource('ship');
 	for my $s (sort { $ships{$b} <=> $ships{$a} } keys %ships) {
-		printf "%3d%% - %s\n", $ships{$s}, resName($ships->{$s});
+		printf "%3d%% - %s (%d)\n", $ships{$s}, resName($ships->{$s}),
+			$ships->{$s}{Strength};
 	}
 	printf "\nStrength: %.2f\n", scalar(dudeStrength($dude));
 }

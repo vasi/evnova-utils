3bd49d9eb673efab2bd0a4164ccb14c884d96e08
Dave Vasilevsky <dave@vasilevsky.ca>
2023-03-21 02:00:53 -0400

    add range to dps

--- Scripts/lib/Nova/Old/outf.pl
+++ Scripts/lib/Nova/Old/outf.pl
@@ -10,17 +10,25 @@ sub dps {
     return $shot / 100 * 30 / $reload;
 }
 
+sub weapRange {
+    my ($weap) = @_;
+    if ($weap->{BeamLength} > 0) {
+        return $weap->{BeamLength};
+    }
+    return $weap->{Speed} * $weap->{Count} / 100;
+}
+
 sub showDPS {
     my ($shield) = 50;
     moreOpts(\@_,
         'armor|a:100' => sub { $shield = 100 - $_[1] },
         'shield|s:100' => sub { $shield = $_[1] });
 
-    rankHeaders(qw(EnergyDmg MassDmg Reload));
+    rankHeaders(qw(EnergyDmg MassDmg Reload Range));
     listBuildSub(type => 'weap',
         value => sub { sprintf "%.1f", dps(\%::r, $shield) },
         filter => sub { ~$::r{Flags} & 2 },
-        print => sub { @::r{qw(EnergyDmg MassDmg Reload)} }
+        print => sub { @::r{qw(EnergyDmg MassDmg Reload)}, weapRange(\%::r) }
     );
 }
 
